3ec13a696b23215672a242502161528844c93a80
Jetroid <holtjet@gmail.com>
2015-08-23 21:40:40 +0100

    Corrected dependency.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,16 @@
 # For dconf-related shenanigans, we need to make sure the profile and
 # directories are created first
 class dconf_profile {
-	
+
+  package{'dconf-cli':
+    ensure => present,
+  }
+
   $dconf_directories = ['/etc/dconf','/etc/dconf/db','/etc/dconf/db/local.d','/etc/dconf/profile','/etc/dconf/db/local.d/locks']
 
   file{$dconf_directories:
     ensure => directory,
   } -> 
-  
-  package{'dconf-cli':
-    ensure => present,
-  }
 
   file{'/etc/dconf/profile/user':
     ensure => present,
